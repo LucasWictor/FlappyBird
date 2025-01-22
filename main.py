6d6b59183ccb9e90610ccbd2aed13709a5cf512d
LucasWictor <Dahlulle@gmail.com>
2025-01-22 18:33:39 +0100

    Added bird logic for reset and flip

--- main.py
+++ main.py
@@ -58,4 +58,50 @@ class Bird(pygame.sprite.Sprite):
 		self.clicked = False
 
         def update(self):
-            if flying == True;
+			if flying == True:
+				# gravity
+				self.vel += 0.5
+				if self.vel > 8:
+					self.vel = 8
+				if self.rect.bottom < 768:
+					self.rect.y += int(self.vel)
+					#jump
+					if game_over == False;
+					if pygame.mouse.get.pressed()[0] == 1 and self.clicked == False:
+						self.clicked = True
+						self.vel = -10
+					if pygame.mouse.get.pressed()[0] == 0:
+						self.clicked = False
+						# Animation
+						self.counter += 1
+						flap_cooldown = 5
+
+						if self.counter > flap_cooldown:
+							self.counter = 0
+							self.index += 1
+							if self.index >= len(self.images):
+								self.index = 0
+							self.image = self.images[self.index]
+
+							#rotate bird
+						self.image = pygame.transform.rotate(self.image, self.vel * -2)
+					else:
+						self.image = pygame.transform.rotate(self.image, self.vel * -90)
+
+class Pipe(pygame.sprite.Sprite):
+	def __init__(self, x, y, position):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.image.load('img/pipe.png')
+		self.rect = self.image.get_rect()
+		#pos 1 is the top, -1 is the the bottom
+		if position == 1:
+			self.image = pygame.transform.flip(self.image, False, True)
+			self.rect.bottomleft = [x, y - int(pipe_gap / 2)]
+		if position == -1:
+			self.rect.topleft = [x, y + int(pipe_gap / 2)]
+
+			def update(self):
+				self.rect.x -= scroll_speed
+				if self.rect.right < 0:
+					self.kill()
+
